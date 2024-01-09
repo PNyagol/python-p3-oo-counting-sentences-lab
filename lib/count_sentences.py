e080bc72121aa243fa398edecf29dd6ed8ee7933
Nyagol <nyagolpeter@gmail.com>
2024-01-09 07:41:22 +0300

    done

--- lib/count_sentences.py
+++ lib/count_sentences.py
@@ -1,4 +1,21 @@
-#!/usr/bin/env python3
+import re
 
 class MyString:
-  pass
+    def __init__(self, value=""):
+        if type(value) is not str:
+            print("The value must be a string.")
+            return
+        self.value = value
+
+    def is_sentence(self):
+        return self.value.endswith('.')
+
+    def is_question(self):
+        return self.value.endswith('?')
+
+    def is_exclamation(self):
+        return self.value.endswith('!')
+
+    def count_sentences(self):
+        sentences = [sentence for sentence in re.split(r'[.!?]', self.value) if sentence]
+        return len(sentences)
